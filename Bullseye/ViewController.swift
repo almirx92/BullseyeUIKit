80641f642f9d2d2013a4219912820f10668e8469
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-02-26 15:44:32 +0100

    Basic Controls

---
+++ Bullseye/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Bullseye
+//
+//  Created by Macbook on 24. 2. 2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert(){
+        let alert = UIAlertController(title: "Hello world!", message: "This is my first app", preferredStyle: .alert);
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil);
+        
+        alert.addAction(action);
+        
+        present(alert, animated: true, completion: nil);
+    }
+    
+    
+}
+
+
